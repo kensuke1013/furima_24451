d984f8a3659fca81bebb3220be6c45de62adce50
kensuke10103 <senjukensuke1010@icloud.com>
2020-10-27 12:02:31 +0900

    ユーザー管理機能　修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-         VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
-         validates :password, presence: true, length: { minimum: 6 }, confirmation: true, format:{ with: VALID_PASSWORD_REGEX}
 
-         validates :nickname,               presence: true
-         validates :encrypted_password,     presence: true
-         validates :family_name,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ここがユーザー本名全角の正規表現
-         validates :family_name_kana,       presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # ここがフリガナ全角の正規表現
-         validates :first_name,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } # ここがユーザー本名全角の正規表現
-         validates :first_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } # ここがフリガナ全角の正規表現
-         validates :birth_day,              presence: true
+        with_options presence: true do
+
+         validates :nickname
+         validates :birth_day
+         
+         VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
+         validates :password, length: { minimum: 6 }, confirmation: true, format:{ with: VALID_PASSWORD_REGEX}
+         validates :password_confirmation
+
+         with_options format: {with: /\A[ぁ-んァ-ン一-龥]/ } do
+          validates :family_name             # ここがユーザー本名全角の正規表現
+          validates :first_name              # ここがユーザー本名全角の正規表現
+         end
+
+         with_options format: {with: /\A[ァ-ヶー－]+\z/ } do
+          validates :family_name_kana        # ここがフリガナ全角の正規表現
+          validates :first_name_kana         # ここがフリガナ全角の正規表現
+         end
+
+         #validates :email,uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+        end
     end
--- db/migrate/20201022030627_devise_create_users.rb
+++ db/migrate/20201022030627_devise_create_users.rb
@@ -4,6 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false, defalut: ""
+      t.string :family_name,        null: false, defalut: ""
+      t.string :family_name_kana,   null: false, defalut: ""
+      t.string :first_name,         null: false, defalut: ""
+      t.string :first_name_kana,    null: false, defalut: ""
+      t.date :birth_day,            null: false, defalut: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
