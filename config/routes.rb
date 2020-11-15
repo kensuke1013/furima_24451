7d3309838b19434b65c704356ea1b0a2dbdb129e
kensuke10103 <senjukensuke1010@icloud.com>
2020-11-15 19:48:31 +0900

    商品出品機能修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  #get 'articles/index'
-  #get 'articles/new'
-
-  #resources :articles
   devise_for :users
-
-  
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   root 'items#index'
   resources :items, only: [:new, :create, :edit, :update]
 
